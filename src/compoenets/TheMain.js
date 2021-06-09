8493c8761927b687033ef6bb23e9f3433b4fe9ec
yousef <yousef0sarkar@gmail.com>
2021-06-09 13:59:55 +0300

    fix with github

---
+++ src/compoenets/TheMain.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Coursal from './coursal'
+ class TheMain extends Component {
+    render() {
+        return (
+            <div >
+            <Coursal/>
+   
+            </div>
+        )
+    }
+}
+
+
+export default TheMain ; 
